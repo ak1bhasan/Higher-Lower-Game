b0d14339e19703a4ab482b7a9bb753d7d260526e
ak1bhasan <akibhasan011@gmail.com>
2025-10-08 03:22:49 +0600

    Added follower count function

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from game_data import data
 import random
 
 
-
 def format_data(account):
-    """Format the account data intro printable format."""
+    """Takes the account data and returns the printable format."""
     account_name = account["name"]
     account_description = account["description"]
     account_country = account["country"]
 
-    print(f"{account_name}, a {account_description}, from {account_country}")
+    return f"{account_name}, a {account_description}, from {account_country}."
+
+
+def check_answer(user_guess, a_followers, b_followers):
+    """Take a user's guess and the follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
 
 
 print(art.logo)
@@ -22,5 +29,16 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}.")
+print(art.vs)
 print(f"Compare B: {format_data(account_b)}.")
+
+
+guess = input("Who has more followers? Type 'A' or 'B' : ").lower()
+
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
 
