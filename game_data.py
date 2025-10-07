a9e54c8485ceb5eeb0cdffbbca8b2d0ecb5ed114
ak1bhasan <akibhasan011@gmail.com>
2025-10-08 02:51:44 +0600

    updating game data

---
+++ game_data.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 615,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 495,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 430,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400,
+        "description": "Reality TV star and entrepreneur",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 390,
+        "description": "Actor and former professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 380,
+        "description": "Singer and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360,
+        "description": "Reality TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 310,
+        "description": "Singer-songwriter",
+        "country": "United States"
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 270,
+        "description": "Cricketer",
+        "country": "India"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 320,
+        "description": "Singer and performer",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 290,
+        "description": "Singer",
+        "country": "Canada"
+    },
+    {
+        "name": "Kendall Jenner",
+        "follower_count": 295,
+        "description": "Model and TV personality",
+        "country": "United States"
+    },
+    {
+        "name": "Neymar Jr",
+        "follower_count": 220,
+        "description": "Footballer",
+        "country": "Brazil"
+    },
+    {
+        "name": "Nicki Minaj",
+        "follower_count": 230,
+        "description": "Rapper and singer",
+        "country": "Trinidad and Tobago"
+    },
+    {
+        "name": "Zendaya",
+        "follower_count": 190,
+        "description": "Actress and singer",
+        "country": "United States"
+    },
+    {
+        "name": "Drake",
+        "follower_count": 150,
+        "description": "Rapper and singer",
+        "country": "Canada"
+    },
+    {
+        "name": "Billie Eilish",
+        "follower_count": 120,
+        "description": "Singer-songwriter",
+        "country": "United States"
+    },
+    {
+        "name": "Emma Watson",
+        "follower_count": 75,
+        "description": "Actress and activist",
+        "country": "United Kingdom"
+    },
+    {
+        "name": "Shakira",
+        "follower_count": 95,
+        "description": "Singer and dancer",
+        "country": "Colombia"
+    },
+    {
+        "name": "Robert Downey Jr.",
+        "follower_count": 65,
+        "description": "Actor",
+        "country": "United States"
+    }
+]
