2ebb8e0d8cd2a79a085c8124871512f923e32937
ak1bhasan <akibhasan011@gmail.com>
2025-10-08 09:53:52 +0600

    Final version

--- main.py
+++ main.py
@@ -21,24 +21,39 @@ def check_answer(user_guess, a_followers, b_followers):
 
 
 print(art.logo)
-
-account_a = random.choice(data)
+score = 0
+game_should_continue = True
 account_b = random.choice(data)
 
-if account_a == account_b:
+
+while game_should_continue:
+    account_a = account_b
     account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(art.vs)
-print(f"Compare B: {format_data(account_b)}.")
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(art.vs)
+    print(f"Compare B: {format_data(account_b)}.")
 
 
-guess = input("Who has more followers? Type 'A' or 'B' : ").lower()
+    guess = input("Who has more followers? Type 'A' or 'B' : ").lower()
 
+    print("\n" * 20)
+    print(art.logo)
 
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"You're are right! Current score: {score}")
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_should_continue = False
+
 
 
